37ca036bab77bcdb3c63ce734cbdc7d035b6d706
ssd352 <27953063+ssd352@users.noreply.github.com>
2018-06-30 19:57:31 -0600

    Initial Commit

---
+++ MacSleeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacSleeper
+//
+//  Created by SS D on 2018-06-30.
+//  Copyright © 2018 SS D. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
